cdbf28fc5085f3cd8ed6827fd950dd6d0358d0cb
shawn2093 <shawnong1993@gmail.com>
2024-01-30 23:54:58 +0800

    tryout

--- lex.c
+++ lex.c
@@ -2,5 +2,27 @@
 
 int main(int ac, char **av)
 {
+    t_lex   *a;
+    t_lex   *chain;
+    char    **tokens;
+    int     i;
+    int     j;
 
+    tokens = (char **)malloc(sizeof(char *) * (ac - 1));
+    i = 0;
+    while (++i < ac)
+    {
+        j = 0;
+        if (av[i][j] == '<' && av[i][j + 1] == '<')
+            tokens[i - 1] = "LESSLESS";
+        else if (av[i][j] == '>' && av[i][j + 1] == '>')
+            tokens[i - 1] = "GREATGREAT";
+        else if (av[i][j] == '<')
+            tokens[i - 1] = "LESS";
+        else if (av[i][j] == '>')
+            tokens[i - 1] = "GREAT";
+        else if (av[i][j] == '|')
+            tokens[i - 1] = "PIPE";
+        
+    }
 }
--- minishell.h
+++ minishell.h
@@ -2,8 +2,11 @@
 # define MINISHELL_H
 
 typedef struct s_lex {
-    char *great;
-    char *less;
-    char *great2;
-    char *less2;
+    char            *great;
+    char            *less;
+    char            *great2;
+    char            *less2;
+    struct s_lex    *next;
 } t_lex;
+
+#endif
